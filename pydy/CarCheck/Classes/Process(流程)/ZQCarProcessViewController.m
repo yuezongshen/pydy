136d1485918b65a12e40bf424d458df6c78c7429
FYXJ（6） <yue@fyxj6deimac.local>
2018-08-09 18:20:34 +0800

    删除高德SDK

--- pydy/CarCheck/Classes/Process(流程)/ZQCarProcessViewController.m
+++ pydy/CarCheck/Classes/Process(流程)/ZQCarProcessViewController.m
@@ -28,15 +28,15 @@
 
 #import "ZQLoginViewController.h"
 
-#import <AMapLocationKit/AMapLocationKit.h>
-#import <AMapFoundationKit/AMapFoundationKit.h>
+//#import <AMapLocationKit/AMapLocationKit.h>
+//#import <AMapFoundationKit/AMapFoundationKit.h>
 
 #import "ZQHtmlViewController.h"
 
 #define DefaultLocationTimeout 10
 #define DefaultReGeocodeTimeout 5
 
-@interface ZQCarProcessViewController()<UITableViewDelegate,UITableViewDataSource,ZQProcessRightCellDelegate,ZQProcessCellDelegate,AMapLocationManagerDelegate>{
+@interface ZQCarProcessViewController()<UITableViewDelegate,UITableViewDataSource,ZQProcessRightCellDelegate,ZQProcessCellDelegate>{
     
     NSMutableArray *_dataArray;
     NSMutableArray *_colorArray;
@@ -46,9 +46,9 @@
 
 @property(strong,nonatomic)UITableView *tableView;
 
-@property (nonatomic, strong) AMapLocationManager *locationManager;
-
-@property (nonatomic, copy) AMapLocatingCompletionBlock completionBlock;
+//@property (nonatomic, strong) AMapLocationManager *locationManager;
+//
+//@property (nonatomic, copy) AMapLocatingCompletionBlock completionBlock;
 @end
 
 @implementation ZQCarProcessViewController
@@ -57,45 +57,45 @@
     [super viewDidLoad];
     [self initViews];
     
-    [AMapServices sharedServices].apiKey = @"38213c630c734efe09f2259bd241cfd2";
-
-    [self initCompleteBlock];
-    
-    [self configLocationManager];
-    
-    [self locAction];
+//    [AMapServices sharedServices].apiKey = @"38213c630c734efe09f2259bd241cfd2";
+//
+//    [self initCompleteBlock];
+//
+//    [self configLocationManager];
+//
+//    [self locAction];
 }
 #pragma mark - Action Handle
 
-- (void)configLocationManager
-{
-    self.locationManager = [[AMapLocationManager alloc] init];
-    
-    [self.locationManager setDelegate:self];
-    
-    //设置期望定位精度
-    [self.locationManager setDesiredAccuracy:kCLLocationAccuracyHundredMeters];
-    
-    //设置不允许系统暂停定位
-    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
-    
-    //设置允许在后台定位
-//    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
-    
-    //设置定位超时时间
-    [self.locationManager setLocationTimeout:DefaultLocationTimeout];
-    
-    //设置逆地理超时时间
-    //    [self.locationManager setReGeocodeTimeout:DefaultReGeocodeTimeout];
-}
-
-- (void)cleanUpAction
-{
-    //停止定位
-    [self.locationManager stopUpdatingLocation];
-    
-    [self.locationManager setDelegate:nil];
-}
+//- (void)configLocationManager
+//{
+//    self.locationManager = [[AMapLocationManager alloc] init];
+//
+//    [self.locationManager setDelegate:self];
+//
+//    //设置期望定位精度
+//    [self.locationManager setDesiredAccuracy:kCLLocationAccuracyHundredMeters];
+//
+//    //设置不允许系统暂停定位
+//    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
+//
+//    //设置允许在后台定位
+////    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
+//
+//    //设置定位超时时间
+//    [self.locationManager setLocationTimeout:DefaultLocationTimeout];
+//
+//    //设置逆地理超时时间
+//    //    [self.locationManager setReGeocodeTimeout:DefaultReGeocodeTimeout];
+//}
+//
+//- (void)cleanUpAction
+//{
+//    //停止定位
+//    [self.locationManager stopUpdatingLocation];
+//
+//    [self.locationManager setDelegate:nil];
+//}
 
 //- (void)reGeocodeAction
 //{
@@ -106,54 +106,54 @@
 - (void)locAction
 {
     //进行单次定位请求
-    [self.locationManager requestLocationWithReGeocode:NO completionBlock:self.completionBlock];
+//    [self.locationManager requestLocationWithReGeocode:NO completionBlock:self.completionBlock];
 }
 #pragma mark - Initialization
 
 - (void)initCompleteBlock
 {
     //    __weak ZQInspectionListController *weakSelf = self;
-    self.completionBlock = ^(CLLocation *location, AMapLocationReGeocode *regeocode, NSError *error)
-    {
-        if (error != nil && error.code == AMapLocationErrorLocateFailed)
-        {
-            //定位错误：此时location和regeocode没有返回值，不进行annotation的添加
-            NSLog(@"定位错误:{%ld - %@};", (long)error.code, error.localizedDescription);
-            return;
-        }
-        else if (error != nil
-                 && (error.code == AMapLocationErrorReGeocodeFailed
-                     || error.code == AMapLocationErrorTimeOut
-                     || error.code == AMapLocationErrorCannotFindHost
-                     || error.code == AMapLocationErrorBadURL
-                     || error.code == AMapLocationErrorNotConnectedToInternet
-                     || error.code == AMapLocationErrorCannotConnectToHost))
-        {
-            //逆地理错误：在带逆地理的单次定位中，逆地理过程可能发生错误，此时location有返回值，regeocode无返回值，进行annotation的添加
-            NSLog(@"逆地理错误:{%ld - %@};", (long)error.code, error.localizedDescription);
-        }
-        else if (error != nil && error.code == AMapLocationErrorRiskOfFakeLocation)
-        {
-            //存在虚拟定位的风险：此时location和regeocode没有返回值，不进行annotation的添加
-            NSLog(@"存在虚拟定位的风险:{%ld - %@};", (long)error.code, error.localizedDescription);
-            return;
-        }
-        else
-        {
-            //没有错误：location有返回值，regeocode是否有返回值取决于是否进行逆地理操作，进行annotation的添加
-        }
-        
-        //修改label显示内容
-        if (regeocode)
-        {
-            NSLog(@"定位显示的内容:%@",[NSString stringWithFormat:@"%@ \n %@-%@-%.2fm", regeocode.formattedAddress,regeocode.citycode, regeocode.adcode, location.horizontalAccuracy]);
-        }
-        else
-        {
-            NSLog(@"定位成功经纬度内容:%@",[NSString stringWithFormat:@"lat:%f;lon:%f \n accuracy:%.2fm", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy]);
-            [Utility saveLongitude:location.coordinate.longitude Latitude:location.coordinate.latitude];
-        }
-    };
+//    self.completionBlock = ^(CLLocation *location, AMapLocationReGeocode *regeocode, NSError *error)
+//    {
+//        if (error != nil && error.code == AMapLocationErrorLocateFailed)
+//        {
+//            //定位错误：此时location和regeocode没有返回值，不进行annotation的添加
+//            NSLog(@"定位错误:{%ld - %@};", (long)error.code, error.localizedDescription);
+//            return;
+//        }
+//        else if (error != nil
+//                 && (error.code == AMapLocationErrorReGeocodeFailed
+//                     || error.code == AMapLocationErrorTimeOut
+//                     || error.code == AMapLocationErrorCannotFindHost
+//                     || error.code == AMapLocationErrorBadURL
+//                     || error.code == AMapLocationErrorNotConnectedToInternet
+//                     || error.code == AMapLocationErrorCannotConnectToHost))
+//        {
+//            //逆地理错误：在带逆地理的单次定位中，逆地理过程可能发生错误，此时location有返回值，regeocode无返回值，进行annotation的添加
+//            NSLog(@"逆地理错误:{%ld - %@};", (long)error.code, error.localizedDescription);
+//        }
+//        else if (error != nil && error.code == AMapLocationErrorRiskOfFakeLocation)
+//        {
+//            //存在虚拟定位的风险：此时location和regeocode没有返回值，不进行annotation的添加
+//            NSLog(@"存在虚拟定位的风险:{%ld - %@};", (long)error.code, error.localizedDescription);
+//            return;
+//        }
+//        else
+//        {
+//            //没有错误：location有返回值，regeocode是否有返回值取决于是否进行逆地理操作，进行annotation的添加
+//        }
+//
+//        //修改label显示内容
+//        if (regeocode)
+//        {
+//            NSLog(@"定位显示的内容:%@",[NSString stringWithFormat:@"%@ \n %@-%@-%.2fm", regeocode.formattedAddress,regeocode.citycode, regeocode.adcode, location.horizontalAccuracy]);
+//        }
+//        else
+//        {
+//            NSLog(@"定位成功经纬度内容:%@",[NSString stringWithFormat:@"lat:%f;lon:%f \n accuracy:%.2fm", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy]);
+//            [Utility saveLongitude:location.coordinate.longitude Latitude:location.coordinate.latitude];
+//        }
+//    };
 }
 
 #pragma mark -AMapLocationManagerDelegate-
@@ -162,10 +162,10 @@
  *  @param manager 定位 AMapLocationManager 类。
  *  @param error 返回的错误，参考 CLError 。
  */
-- (void)amapLocationManager:(AMapLocationManager *)manager didFailWithError:(NSError *)error
-{
-    NSLog(@"定位失败:error:%@",[error description]);
-}
+//- (void)amapLocationManager:(AMapLocationManager *)manager didFailWithError:(NSError *)error
+//{
+//    NSLog(@"定位失败:error:%@",[error description]);
+//}
 #pragma mark 私有方法
 -(void)initViews {
     
--- pydy/Pods/Target Support Files/Pods-CarCheck/Pods-CarCheck-acknowledgements.markdown
+++ pydy/Pods/Target Support Files/Pods-CarCheck/Pods-CarCheck-acknowledgements.markdown
@@ -24,16 +24,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 
-## AMapFoundation-NO-IDFA
-
-Copyright © 2014 AutoNavi. All Rights Reserved.
-
-
-## AMapLocation-NO-IDFA
-
-Copyright © 2014 AutoNavi. All Rights Reserved.
-
-
 ## BMKLocationKit-NO-IDFA
 
 MIT License
